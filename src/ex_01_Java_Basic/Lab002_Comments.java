cec89d3abefe7949574c24f3e75a04a8baef18e3
Kathir <kathir300499@gmail.com>
2025-03-10 09:45:45 +0530

    10th March

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        //This is a single line comment
+        /*This is a multiline comment
+        Name - Kathiravan
+        Comments are non-executable piece of code
+         */
+        System.out.println("This is a comment class");
+    }
+}
