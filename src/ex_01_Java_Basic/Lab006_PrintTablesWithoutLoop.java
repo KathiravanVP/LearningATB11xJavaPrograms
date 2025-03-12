031e9ac6354671e90b39affbd0dcbdfa07541287
Kathir <kathir300499@gmail.com>
2025-03-12 08:15:46 +0530

    12th March

--- src/ex_01_Java_Basic/Lab006_PrintTablesWithoutLoop.java
+++ src/ex_01_Java_Basic/Lab006_PrintTablesWithoutLoop.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Lab006_PrintTablesWithoutLoop {
     public static void main(String[] args) {
         int table=9;
-        System.out.printf("%d x %d = %d\n",table,1,table*1);
-        System.out.printf("%d x %d = %d\n",table,2,table*2);
-        System.out.printf("%d x %d = %d\n",table,3,table*3);
-        System.out.printf("%d x %d = %d\n",table,4,table*4);
-        System.out.printf("%d x %d = %d\n",table,5,table*5);
-        System.out.printf("%d x %d = %d\n",table,6,table*6);
-        System.out.printf("%d x %d = %d\n",table,7,table*7);
-        System.out.printf("%d x %d = %d\n",table,8,table*8);
-        System.out.printf("%d x %d = %d\n",table,9,table*9);
-        System.out.printf("%d x %d = %d",table,10,table*10);
+        System.out.printf("%d x 1 = %d\n",table,table*1);
+        System.out.printf("%d x 2 = %d\n",table,table*2);
+        System.out.printf("%d x 3 = %d\n",table,table*3);
+        System.out.printf("%d x 4 = %d\n",table,table*4);
+        System.out.printf("%d x 5 = %d\n",table,table*5);
+        System.out.printf("%d x 6 = %d\n",table,table*6);
+        System.out.printf("%d x 7 = %d\n",table,table*7);
+        System.out.printf("%d x 8 = %d\n",table,table*8);
+        System.out.printf("%d x 9 = %d\n",table,table*9);
+        System.out.printf("%d x 10 = %d",table,table*10);
     }
 }
