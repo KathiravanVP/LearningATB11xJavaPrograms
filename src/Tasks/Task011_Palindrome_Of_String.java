a8fb83b4350be9ecbfbe04e223b489e089f6a835
Kathir <kathir300499@gmail.com>
2025-03-22 17:33:41 +0530

    22nd March

---
+++ src/Tasks/Task011_Palindrome_Of_String.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task011_Palindrome_Of_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string to check palindrome: ");
+        String str = sc.next();
+        String rev_str = "";
+        for(int i=str.length()-1;i>=0;i--){
+            rev_str = rev_str+str.charAt(i);
+        }
+        System.out.println(rev_str);
+        if(str.equals(rev_str)){
+            System.out.println("String is a palindrome");
+        }
+        else{
+            System.out.println("String is not a palindrome");
+        }
+    }
+}
