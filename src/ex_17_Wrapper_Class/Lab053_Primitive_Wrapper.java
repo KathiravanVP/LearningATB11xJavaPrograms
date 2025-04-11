a57c6c4a03be110e887d327438e30929f90ba42a
Kathir <kathir300499@gmail.com>
2025-04-11 07:03:48 +0530

    11th April

---
+++ src/ex_17_Wrapper_Class/Lab053_Primitive_Wrapper.java
@@ -0,0 +1,19 @@
+package ex_17_Wrapper_Class;
+
+public class Lab053_Primitive_Wrapper {
+    public static void main(String[] args){
+        int a = 10;
+        Integer age = 65;
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(age);
+        System.out.println(age.intValue());
+        System.out.println("-----------------------");
+        Integer b = a;
+        System.out.println(b);
+        System.out.println("-----------------------");
+        Integer c = 20;
+        int d = c;
+        System.out.println(d);
+    }
+}
